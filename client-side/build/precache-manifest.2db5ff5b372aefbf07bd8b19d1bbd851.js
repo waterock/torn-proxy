32ce95ff8a7d79a36e73698c044e7dc7f373346b
sulsay <sulsay@gmail.com>
2020-07-31 16:17:54 +0200

    Include production build in version control for now

---
+++ client-side/build/precache-manifest.2db5ff5b372aefbf07bd8b19d1bbd851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee44abdb6b7a5e3d6ad6e1a7720b412b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27c504c5fcd75b9ce66",
+    "url": "/static/css/main.4f0b7f3c.chunk.css"
+  },
+  {
+    "revision": "4eb8410c416367f732ce",
+    "url": "/static/js/2.6155498d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6155498d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27c504c5fcd75b9ce66",
+    "url": "/static/js/main.d3e295f6.chunk.js"
+  },
+  {
+    "revision": "6af373bd941c439ec83d",
+    "url": "/static/js/runtime-main.d7af95be.js"
+  }
+]);
