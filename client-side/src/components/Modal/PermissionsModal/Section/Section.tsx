1985e641f7572dda6be5a38bfa5992a4d1e4ea0f
sulsay <sulsay@gmail.com>
2020-08-25 15:23:10 +0200

    Test with actual selections

--- client-side/src/components/Modal/PermissionsModal/Section/Section.tsx
+++ client-side/src/components/Modal/PermissionsModal/Section/Section.tsx
@@ -1,68 +1,78 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './Section.module.scss';
 import SectionInterface from './SectionInterface';
 
 interface Props {
-    section: SectionInterface
-    nestingLevel?: number
+  section: SectionInterface
+  nestingLevel?: number
 }
 
 const Section: FC<Props> = ({ section, nestingLevel = 0 }) => {
 
-    function renderChildSections() {
-        if (!section.childSections || section.childSections.length === 0) {
-            return;
-        }
-        return section.childSections.map((childSection) => (
-            <Section key={childSection.label} section={childSection} nestingLevel={nestingLevel + 1}/>
-        ));
+  const [allowAll, setAllowAll] = useState<boolean>(false);
+
+  function renderChildSections() {
+    if (!section.childSections || section.childSections.length === 0) {
+      return;
+    }
+    return section.childSections.map((childSection) => (
+      <Section key={childSection.label} section={childSection} nestingLevel={nestingLevel + 1} />
+    ));
+  }
+
+  function renderPermissions() {
+    if (!section.permissions || section.permissions.length === 0) {
+      return;
     }
 
-    function renderPermissions() {
-        if (!section.permissions || section.permissions.length === 0) {
-            return;
-        }
+    return section.permissions.map(([permission, granted]) => {
+      return (
+        <Section
+          key={permission}
+          section={{
+            label: permission,
+            granted,
+          }}
+          nestingLevel={nestingLevel + 1}
+        />
+      );
+    });
+  }
 
-        return section.permissions.map(([permission, granted]) => {
-            return (
-                <Section
-                    key={permission}
-                    section={{
-                        label: permission,
-                        granted,
-                    }}
-                    nestingLevel={nestingLevel + 1}
-                />
-            );
-        });
+  function nest(value: any) {
+    for (let i = 0; i < nestingLevel; i++) {
+      value = <div className={styles.nesting}>{value}</div>;
     }
+    return value;
+  }
 
-    function nest(value: any) {
-        for (let i = 0; i < nestingLevel; i++) {
-            value = <div className={styles.nesting}>{value}</div>;
-        }
-        return value;
+  function renderSectionGrantContents() {
+    if (section.granted === undefined) {
+      return (
+        <label>
+          <input type="checkbox" checked={allowAll} onChange={(event) => setAllowAll(event.target.checked)} /> allow all
+        </label>
+      );
     }
+    return <input type="checkbox" checked={section.granted} readOnly />
+  }
 
-    function renderSectionGrantContents() {
-        if (section.granted === undefined) {
-            return null;
-        }
-        return <input type="checkbox" checked={section.granted} readOnly/>
-    }
-
-    return nest(
+  return nest(
+    <>
+      <div className={styles.root}>
+        <div className={styles.sectionLabel}>{section.label}</div>
+        <div className={styles.sectionGrant}>
+          {renderSectionGrantContents()}
+        </div>
+      </div>
+      {!allowAll && (
         <>
-            <div className={styles.root}>
-                <div className={styles.sectionLabel}>{section.label}</div>
-                <div className={styles.sectionGrant}>
-                    {renderSectionGrantContents()}
-                </div>
-            </div>
-            {renderChildSections()}
-            {renderPermissions()}
+          {renderChildSections()}
+          {renderPermissions()}
         </>
-    )
+      )}
+    </>
+  )
 };
 
 export default Section;
