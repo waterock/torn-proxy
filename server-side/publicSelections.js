f35b0087de600d12b03b4b7bb3f1ce05e01aaa0f
sulsay <sulsay@gmail.com>
2020-10-18 12:23:43 +0200

    Finish the whitelist of public selections

--- server-side/publicSelections.js
+++ server-side/publicSelections.js
@@ -1,4 +1,8 @@
 module.exports = {
-    // todo list all public selections by resource
-    user: ['', 'basic', 'bazaar', 'crimes', 'discord', '...', 'profile', '...'],
+    user: ['', 'basic', 'bazaar', 'crimes', 'discord', 'display', 'profile', 'properties', 'timestamp'],
+    property: ['', 'property', 'timestamp'],
+    faction: ['', 'basic', 'chain', 'territory', 'timestamp'],
+    company: ['', 'employees', 'profile', 'timestamp'],
+    market: ['', 'bazaar', 'itemmarket', 'pointsmarket', 'timestamp'],
+    torn: ['', 'bank', 'companies', 'competition', 'education', 'factiontree', 'gyms', 'honors', 'items', 'medals', 'organisedcrimes', 'pawnshop', 'properties', 'rackets', 'raids', 'stats', 'stocks', 'territory', 'territorywars', 'timestamp'],
 };
