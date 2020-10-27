f4b7fbbaa6a2360aa7667e7388a1f74d23833644
Wouter Alberts <wouter.alberts@teamrockstars.nl>
2020-10-27 12:55:14 +0100

    Fix proxying full path instead of only the part before the first slash

--- server-side/middlewares/getTornRequestPath.js
+++ server-side/middlewares/getTornRequestPath.js
@@ -3,7 +3,7 @@ const getParamsWithReplacedKey = require('../getParamsWithReplacedKey');
 module.exports = (req, res, next) => {
     req.locals = {
         ...req.locals,
-        proxyPath: `/${req.locals.resource}?${getParamsWithReplacedKey(req).join('&')}`,
+        proxyPath: `${req.path}?${getParamsWithReplacedKey(req).join('&')}`,
     };
     next();
 };
